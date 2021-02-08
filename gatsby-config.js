d7d23bc0290ba4dfeaf01baec34e66ebe2c30623
hiteshparate <hiteshparate77@gmail.com>
2021-02-08 11:20:42 -0500

    Change in package

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ require(`dotenv`).config({
 const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
+  pathPrefix: "/gatsbyportfolio",
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
